c47e5874390e7484ecf70058aea52b92ff65f10f
neofrankuy21 <neofrankuy21@gmail.com>
2025-10-25 14:16:16 +0800

    Add new components

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -3,6 +3,7 @@ import AppLayout from '@/layouts/app-layout';
 import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
+import { InputGroup, InputGroupText, InputGroupInput, InputGroupTextarea} from '@/components/ui/input-group';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -16,17 +17,30 @@ export default function Dashboard() {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-                <div className='grid grid-rows-1 grid-cols-2 gap-5 justify-center'>
-                    <Card>
-                        <CardContent>
-                            Hello
-                        </CardContent>
-                    </Card>
-                    <Card>
-                        <CardContent>
-                            Hello
-                        </CardContent>
-                    </Card>
+                <div className='grid grid-rows-1 grid-cols-2 gap-4'>
+                   <div>
+                        <Card>
+                            <CardContent>
+                               <div className='grid grid-row gap-3'>
+                                    <InputGroup label="Email" htmlFor="email">
+                                        <InputGroupInput id="email" type="email" placeholder="Enter your email" />
+                                    </InputGroup>
+                               </div>
+                            </CardContent>
+                        </Card>
+                   </div>
+                   <div>
+                        <Card>
+                            <CardContent>
+                                <InputGroup label="Username" htmlFor="username">
+                                    <InputGroupInput id="username" type="text" placeholder="Enter your username" />
+                                </InputGroup>
+                                <InputGroup label="Bio" htmlFor="bio">
+                                    <InputGroupTextarea id="bio" placeholder="Write a short bio" rows={4} />
+                                </InputGroup>
+                            </CardContent>
+                        </Card>
+                   </div>
                 </div>
             </div>
         </AppLayout>
